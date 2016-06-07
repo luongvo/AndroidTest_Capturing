ae88465761bb7dcb27f7e1fe8ccb14da4978b0ff
Luong Vo <luong.vo@eastagile.com>
2016-06-07 11:05:33 +0700

    Init

---
+++ app/src/main/java/vn/luongvo/androidtest/capturing/MainActivity.java
@@ -0,0 +1,18 @@
+package vn.luongvo.androidtest.capturing;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import vn.luongvo.androidtest.R;
+
+/**
+ * @author Luong
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
