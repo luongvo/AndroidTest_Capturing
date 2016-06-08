5bf052c680d023bd9625fea44c813263d29b8d46
Luong Vo <luong.vo@eastagile.com>
2016-06-08 15:49:29 +0700

    Prepare video folder and wait for video rendering

--- run_test+record.sh
+++ run_test+record.sh
@@ -2,9 +2,13 @@
 FOLDER_NAME="ANDROID_TEST_VIDEO"
 FILE_NAME=".mp4"
 
-# video folder
+# prepare video folder
+# device
 ./adb+.sh shell mkdir /sdcard/$FOLDER_NAME
 ./adb+.sh shell rm -rf /sdcard/$FOLDER_NAME/*
+# local pc
+mkdir ./$FOLDER_NAME
+rm -rf ./$FOLDER_NAME/*
 
 echo
 echo 'SCREEN_RECORD IS STARTING...'
@@ -24,9 +28,14 @@ echo
 echo 'SCREEN_RECORD IS STOPPED...'
 echo "Test video is located at /sdcard/$FOLDER_NAME"
 
+# sleep for video render
+for i in `seq 5 1`;
+do
+    echo "Waiting for video render...$i"
+    sleep 1
+done
+
 # copy video from devices into pc
-mkdir ./$FOLDER_NAME
-rm -rf ./$FOLDER_NAME/*
 ./adb+.sh pull /sdcard/$FOLDER_NAME ./$FOLDER_NAME/
 
 echo "Test video is pulled into ./$FOLDER_NAME"
