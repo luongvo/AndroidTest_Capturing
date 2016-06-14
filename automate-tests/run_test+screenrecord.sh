1e06280de0d305b3e40378718255a49feb23b84f
Luong Vo <luong.vo@eastagile.com>
2016-06-14 13:57:03 +0700

    Update

--- automate-tests/run_test+screenrecord.sh
+++ automate-tests/run_test+screenrecord.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 BASE_DIR="automate-tests"
-FOLDER_NAME="ANDROID_TEST_VIDEO"
-FILE_NAME="_screenrecord.mp4"
+VIDEO_DIR="$BASE_DIR/videos"
+FILE_NAME=".mp4"
 
 # prepare
 cur_dir=$(dirname "$0")
 cd $cur_dir/../$BASE_DIR
 
+rm -R results
 mkdir results
-rm -rf results/*
 mkdir results/logs
 mkdir results/videos
 
@@ -16,12 +16,13 @@ mkdir results/videos
 exec &> >(tee -a "results/logs/run_test+screenrecord.sh.log")
 
 # prepare video folder on devices
-./adb+.sh shell mkdir /sdcard/$FOLDER_NAME
-./adb+.sh shell rm -rf /sdcard/$FOLDER_NAME/*
+./adb+.sh shell mkdir /sdcard/$BASE_DIR
+./adb+.sh shell rm -rf /sdcard/$BASE_DIR/*
+./adb+.sh shell mkdir /sdcard/$VIDEO_DIR
 
 echo
 echo 'SCREEN_RECORD IS STARTING...'
-./adb+nohup.sh "shell screenrecord /sdcard/$FOLDER_NAME" "$FILE_NAME"
+./adb+nohup.sh "shell screenrecord /sdcard/$VIDEO_DIR" "$FILE_NAME"
 
 # call test
 echo
@@ -37,17 +38,17 @@ ps | grep "shell screenrecord"
 ps | grep "shell screenrecord" | grep -v grep | awk '{print $1}' | xargs kill -9
 echo
 echo 'SCREEN_RECORD IS STOPPED...'
-echo "Test video is located at /sdcard/$FOLDER_NAME"
+echo "Test video is located at /sdcard/$VIDEO_DIR"
 
 # sleep for video render
-for i in `seq 5 1`;
+for i in `seq 3 1`;
 do
     echo "Waiting for video render...$i"
     sleep 1
 done
 
 # copy video from devices into pc
-./adb+.sh pull /sdcard/$FOLDER_NAME results/videos
+./adb+.sh pull /sdcard/$VIDEO_DIR results
 
 echo
 echo "Automate tests results is located at ./$BASE_DIR/results in this repo folder"
